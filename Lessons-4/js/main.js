cbc54df3d18195feea1b393a108cf03ebf24a1cb
Alex <adyatlov1985@gmail.com>
2020-12-19 02:59:32 +0200

    dz-3-1

--- Lessons-4/js/main.js
+++ Lessons-4/js/main.js
@@ -2,4 +2,64 @@
 
 // Массивы и объекты
 
-let array = [1, 2, 3, 'a', 'b', 'c', '4', '5', '6'];
+// 1
+
+// ET 15
+// AT 20
+
+// let sum = 0;
+// let arr = [1, 2, 3, 'a', 'b', 'c', '4', '5', '6'];
+
+// for (let i = 0; i < arr.length; i++) {
+// 	if (Number(arr[i])) {
+// 		sum = sum + Number(arr[i]);
+// 	} else {
+// 		continue;
+// 	}
+// }
+
+// alert(sum);
+
+// 2
+
+// ET 15
+// AT 30
+
+// let arr = [];
+
+// for (var i = 0; i < 5; i++) {
+// 	arr.push(Math.random().toFixed(2));
+// }
+
+// console.log(arr);
+
+// for (var i = 0; i < arr.length; i++) {
+// 	arr[i] = arr[i] ** 5;
+
+// 	console.log(arr[i]);
+// }
+
+// 3
+
+// ET 15
+// AT 40
+
+// let arr = ['AngularJS', 'jQuery'];
+
+// arr.unshift('Backbone.js');
+// arr.push('ReactJS', 'Vue.js');
+// arr.splice(1, 0, "CommonJS");
+
+// console.log(arr);
+
+// for (var i = 0; i < arr.length; i++) {
+// 	if (arr[i] === 'jQuery') {
+// 		alert(`Это здесь лишнее ${arr[i]}`);
+
+// 		delete arr[i];
+// 	}
+// }
+
+// arr = arr.filter(Boolean);
+
+// console.log(arr);
