fe48fdb94b7ce4819a9b7375d912db494db49f84
Alex <adyatlov1985@gmail.com>
2020-12-21 03:13:07 +0200

    dz-3-2

--- Lessons-4/js/main.js
+++ Lessons-4/js/main.js
@@ -48,18 +48,139 @@
 
 // arr.unshift('Backbone.js');
 // arr.push('ReactJS', 'Vue.js');
-// arr.splice(1, 0, "CommonJS");
-
+// arr.splice(1, 0, 'CommonJS');
+// alert(`Это здесь лишнее ${arr[3]}`);
+// arr.splice(3, 1);
 // console.log(arr);
 
-// for (var i = 0; i < arr.length; i++) {
-// 	if (arr[i] === 'jQuery') {
-// 		alert(`Это здесь лишнее ${arr[i]}`);
+// 4 ?
 
-// 		delete arr[i];
+// ET 10
+// AT 1
+
+let str = 'Как однажды Жак звонарь сломал фонарь головой';
+let arr = str.split(' ');
+arr.splice(1, 1);
+arr.splice(3, 0, 'однажды');
+arr[1] = arr.splice(1,1, arr[1]);
+// arr.splice(4, 1);
+console.log(arr);
+
+// 5
+// ET 15
+// AT 20
+
+// let property = prompt('Введите свойство', '');
+// let value = '';
+// let person = {
+// 	'name': 'John',
+// 	'age': 23,
+// 	'gender': 'male'
+// }
+
+// if (property in person) {
+// 	console.log(person.name);
+// } else {
+// 	value = prompt('Введите значение свойства', '');
+// 	person[property] = value;
+// };
+
+// console.log(person);
+
+// 6
+
+// ET 10
+// AT 20
+
+// let person = {
+// 	'name': 'John',
+// 	'age': 23,
+// 	'gender': 'male'
+// }
+
+// let phone = {
+// 	brand: prompt('Phone brand'),
+// 	model: prompt('Phone model'),
+// 	resolution: prompt('Phone resolution'),
+// 	color: prompt('Phone color')
+// }
+
+// person['phone'] = phone;
+
+// console.log(person);
+
+// 7
+
+// ET 10
+// AT 30
+
+// let dates = {
+// 	current: new Date(),
+// 	past: Date.parse(new Date()) - (365 * 24 * 3600 * 1000)
+// }
+
+// let userDate = Date.parse(new Date(prompt('Введите дату в формате', '2020-12-20')));
+
+// if (userDate <= dates.current && userDate >= dates.past) {
+// 	alert('Введенная дата попадает в диапазон заданных дат');
+// } else {
+// 	alert('Введенная дата НЕ попадает в диапазон заданных дат');
+// }
+
+// 8
+
+// ET 15
+// AT 20
+
+// let arr = [];
+// let result = 0;
+
+// for (var i = 0; i < 10; i++) {
+// 	let inputValue = prompt('Введите любой символ', '');
+
+// 	arr.push(inputValue);
+// }
+
+// for (var i = 0; i < arr.length; i++) {
+// 	if (Number(arr[i])) {
+// 		result += Number(arr[i]);
 // 	}
 // }
 
-// arr = arr.filter(Boolean);
+// alert(`Сумма чисел = ${result}`);
 
+
+// 9
+
+// ET 15
+// AT 30
+
+// let arr = [];
+// let arrInner = [];
+
+// for (let i = 1; i <= 10; i++) {
+
+// 	for (let j = 1; j <= 10; j++) {
+// 		let c = i * j;
+
+// 		arrInner.push(`${i} * ${j} = ${c}`);
+// 	}
+// }
+
+// for (let i = 0; i < arrInner.length; i += 10) {
+// 	arr.push(arrInner.slice(i, i + 10));
+// }
 // console.log(arr);
+
+// 10
+
+// ET 5
+// AT 15
+
+// let img = {
+// 	tag: 'img',
+// 	src: 'https://www.google.com.ua/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png',
+// 	alt: '',
+// 	style: 'border: 1px solid #ccc',
+// 	width: 200
+// };
