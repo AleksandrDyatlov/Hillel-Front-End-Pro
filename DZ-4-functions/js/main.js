3a058a5088210f7d49da9a28b6f64393f8307efa
Alex <adyatlov1985@gmail.com>
2020-12-29 12:11:14 +0200

    remove folder Lessons-5

---
+++ DZ-4-functions/js/main.js
@@ -0,0 +1,53 @@
+'use strict';
+
+// Функции
+
+// 1
+
+// ET 2
+// AT 3
+
+// let max = (a, b) => {
+// 	if (a > b) {
+// 		return a;
+// 	} else {
+// 		return b;
+// 	}
+// }
+
+// console.log(max(0, -1));
+
+// 2
+
+// ET 20
+// AT 30
+
+// function min(...rest) {
+// 	let result = Infinity;
+
+// 	for (var i = 0; i < rest.length; i++) {
+// 		if (rest[i] < result) {
+// 			result = rest[i];
+// 		}
+// 	}
+
+// 	return result;
+// }
+
+// console.log(min(0, -1, 100, 500, 100500));
+
+// 3
+
+let users = [
+    {firstName: 'John', lastName: 'Snow', age: 16},
+    {firstName: 'Eddart', lastName: 'Stark', age: 30},
+    {firstName: 'Keytlin', lastName: 'Stark', age: 28},
+    {firstName: 'Ricard', lastName: 'Karstark', age: 15},
+    {firstName: 'Rumsy', lastName: 'Snow', age: 24},
+    {firstName: 'Mira', lastName: 'Rid', age: 26},
+    {firstName: 'Leo', lastName: 'Lefford', age: 55},
+    {firstName: 'Robert', lastName: 'Barateon', age: 17},
+    {firstName: 'Merrit', lastName: 'Trunt', age: 42},
+    {firstName: 'Mendon',lastName: 'Moor', age: 38}
+];
+
