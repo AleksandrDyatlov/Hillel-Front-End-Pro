f4a37c2af0c76513472efbd04f936aff7ae76986
Alex <adyatlov1985@gmail.com>
2021-04-06 19:20:16 +0300

    added to do list edit/done

--- My work practice/markup/js/jquery.main.js
+++ My work practice/markup/js/jquery.main.js
@@ -4,50 +4,112 @@ jQuery(function() {
 });
 
 function initToDoList() {
-	const list = jQuery('.todolist');
-	// const listItems = list.find('li');
-	const btnAdd = jQuery('#btn-add');
-	const input = jQuery('#input');
-	let title = '';
+	jQuery('.todolist').each(function() {
+		const list = jQuery(this);
+		const btnAdd = jQuery('#btn-add');
+		const input = jQuery('#input');
+		const doneClass = 'done';
+		let title = '';
 
-	btnAdd.on('click', addItem);
+		hideSaveBtn();
+		btnAdd.on('click', addItem);
 
-	list.on('click', '.btn-del', removeItem);
+		list.on('click', '.btn-del', removeItem);
+		list.on('click', '.btn-edit', editTitle);
+		list.on('click', '.btn-done', eventDone);
 
-	input.keypress(function(e) {
-		if (e.keyCode === 13) {
-			addItem();
+		input.keypress(function(e) {
+			if (e.keyCode === 13) {
+				addItem();
+			}
+		}).keyup(function() {
+			title = input.val();
+		});
+
+		function addItem() {
+			if (title) {
+				list.append(
+					`<li class="px-2 list-group-item">
+						<div class="btn-holder">
+							<button class="btn btn-done" type="button"></button>
+							<button class="btn btn-up" type="button"></button>
+							<button class="btn btn-down" type="button"></button>
+						</div>
+						<h6>${title}</h6>
+						<div class="btn-holder">
+							<button class="btn btn-save" type="button"></button>
+							<button class="btn btn-edit" type="button"></button>
+							<button class="btn btn-del btn-close" type="button"></button>
+						</div>
+					</li>`
+				);
+
+				hideSaveBtn();
+			}
+
+			input.val('');
 		}
-	}).keyup(function() {
-		title = input.val();
+
+		function removeItem(e) {
+			jQuery(e.target).closest('li').remove();
+		}
+
+		function editTitle(e) {
+			const holder = jQuery(e.target).closest('li');
+			const listTitle = holder.find('h6');
+			const btnEdit = holder.find('.btn-edit');
+			const btnSave = holder.find('.btn-save');
+
+			listTitle.attr('contenteditable', true);
+			holder.addClass('editable');
+			btnEdit.hide();
+			btnSave.show();
+
+			btnSave.on('click', function() {
+				listTitle.attr('contenteditable', false);
+				holder.removeClass('editable');
+				btnSave.hide();
+				btnEdit.show();
+			});
+		}
+
+		function eventDone(e) {
+			const holder = jQuery(e.target).closest('li');
+			const buttons = holder.find('button:not(.btn-done):not(.btn-close)');
+
+			if (holder.hasClass(doneClass)) {
+				holder.removeClass(doneClass);
+
+				buttons.each(function() {
+					jQuery(this).show();
+				});
+
+				hideSaveBtn();
+			} else {
+				holder.addClass(doneClass);
+
+				buttons.each(function() {
+					jQuery(this).hide();
+				});
+			}
+		}
+
+		function hideSaveBtn() {
+			jQuery('.btn-save').each(function() {
+				jQuery(this).hide();
+			});
+		}
+
+		function changePosition() {
+			btnUp.on('click', function() {
+			});
+
+			btnDown.on('click', function() {
+			});
+		}
+
+		changePosition();
 	});
-
-	function addItem() {
-		if (title) {
-			list.append(
-				`<li class="ps-3 pe-2 list-group-item">
-					<h6>${title}</h6>
-					<button class="btn btn-del btn-close" type="button"></button>
-				</li>`
-			);
-		}
-
-		input.val('');
-	}
-
-	function removeItem(e) {
-		jQuery(e.target).parent().remove();
-	}
-
-	// jQuery(listItems).each(function() {
-	// 	const holder = jQuery.this;
-	// 	console.log(holder);
-	// 	const btnDel = holder.find('.btn');
-	//
-	// 	btnDel.on('click', function() {
-	// 		this.parent().remove();
-	// 	});
-	// });
 }
 
 // function initTableRow() {
